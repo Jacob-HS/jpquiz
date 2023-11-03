956fbd30761277c4ccfcfd199f7cfa345559a8a2
Jacob-HS <jacobsabbagh99@gmail.com>
2023-11-03 12:22:37 +0900

    thing and fuck

--- backend/api.py
+++ backend/api.py
@@ -20,7 +20,7 @@ AnswerTracker= db.Table("answertracker",
                         db.Column("correctcount", db.Integer, server_default=text("0")),
                         db.Column("incorrectcount", db.Integer, server_default=text("0"))
                         )
-print(Path.cwd() / Path(r"..\frontend\build"))
+print(os.listdir())
 with open("goiryokuQuestions.json", "r", encoding="utf-8") as infile:
     goiryokuQuestions=json.load(infile)
 with open("quizList.json", "r", encoding="utf-8") as infile:
