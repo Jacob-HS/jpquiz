4c1133fe95b78b6d85f5f5d3906308a54fe3fc5a
Jacob-HS <jacobsabbagh99@gmail.com>
2023-11-03 12:31:27 +0900

    reverted

--- backend/api.py
+++ backend/api.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 db=SQLAlchemy()
 DB_NAME="jpquiz.db"
-app = Flask(__name__, static_folder=Path(r"..\frontend\build"), static_url_path="")
+app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
 CORS(app)
 app.config['SECRET_KEY'] = "frick"
 app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://nwjvfrtkyviply:12674594f6cac5865af202d24e918f1e9f41a8662043355c932921c1180333a2@ec2-52-6-117-96.compute-1.amazonaws.com:5432/d4aa6667vpdng7"
@@ -20,7 +20,6 @@ AnswerTracker= db.Table("answertracker",
                         db.Column("correctcount", db.Integer, server_default=text("0")),
                         db.Column("incorrectcount", db.Integer, server_default=text("0"))
                         )
-print(os.listdir(Path(r"frontend\build")))
 with open("goiryokuQuestions.json", "r", encoding="utf-8") as infile:
     goiryokuQuestions=json.load(infile)
 with open("quizList.json", "r", encoding="utf-8") as infile:
